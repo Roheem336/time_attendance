0d6a12aa689216d192a84722055669b903f9dbcb
Roheem336 <roheem30122551@gmail.com>
2025-06-29 21:51:50 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,7 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -8,5 +11,5 @@ $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-session_start();
+
 ?>
